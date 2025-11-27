5b86be51023f66258fd0f48f5e25e3ec44f1bb08
wuttda <kimberlysilvano267@gmail.com>
2025-11-28 07:07:11 +0800

    pinakafinaljs

--- mainapp/static/scripts/login.js
+++ mainapp/static/scripts/login.js
@@ -208,5 +208,8 @@ window.setMode = (mode) => {
 /*
   This is a multi-line comment
   You can write multiple lines here
+*//*
+  This is a multi-line comment
+  You can write multiple lines here
 */
 
