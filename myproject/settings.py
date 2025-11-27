ba9f433f917016eb0ec09cccf72a10c1c9d90045
Mreow8 <honzelchavez100@gmail.com>
2025-11-28 02:38:46 +0800

    fix bugs for deployment

--- myproject/settings.py
+++ myproject/settings.py
@@ -1,45 +1,45 @@
 from pathlib import Path
-from decouple import config, Csv
-import firebase_admin
-from firebase_admin import credentials
-
-import json
-import firebase_admin
-from firebase_admin import credentials
 from decouple import config
-
-if not firebase_admin._apps:
-    cred_json = config("FIREBASE_SERVICE_ACCOUNT_JSON")
-    cred_dict = json.loads(cred_json)
-    cred = credentials.Certificate(cred_dict)
-    firebase_admin.initialize_app(cred)
+import firebase_admin
+from firebase_admin import credentials
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+import os
+import firebase_admin
+from firebase_admin import credentials
 
-# --------------------
-# Security
-# --------------------
-SECRET_KEY = config("SECRET_KEY")
-DEBUG = config("DEBUG", default=False, cast=bool)
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=Csv())
+# ... existing code ...
 
-# --------------------
-# Installed apps
-# --------------------
+# --- FIREBASE SETUP ---
+if not firebase_admin._apps:
+    if os.path.exists('/etc/secrets/serviceAccountKey.json'):
+        cred_path = '/etc/secrets/serviceAccountKey.json'
+    
+    else:
+        cred_path = os.path.join(BASE_DIR, 'firebase', 'serviceAccountKey.json')
+
+    try:
+        cred = credentials.Certificate(cred_path)
+        firebase_admin.initialize_app(cred)
+    except FileNotFoundError:
+        print(f"WARNING: Firebase credentials not found at {cred_path}. Firebase features will fail.")
+SECRET_KEY = 'django-insecure-wms3(k^@+m4#$4&6uy04!nv391qte85+2^6qvk@z42gda5#u#i'
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'pipeline',
+    'mainapp',
     'django.contrib.staticfiles',
-    'pipeline',       # django-pipeline
-    'mainapp',        # your app
 ]
 
-# --------------------
-# Middleware
-# --------------------
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -50,9 +50,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-# --------------------
-# URLs & Templates
-# --------------------
 ROOT_URLCONF = 'myproject.urls'
 
 TEMPLATES = [
@@ -72,9 +69,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'myproject.wsgi.application'
 
-# --------------------
-# Database
-# --------------------
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -82,29 +76,32 @@ DATABASES = {
     }
 }
 
-# --------------------
-# Password validators
-# --------------------
 AUTH_PASSWORD_VALIDATORS = [
-    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',},
-    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',},
-    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',},
-    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',},
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
 ]
 
-# --------------------
-# Localization
-# --------------------
 LANGUAGE_CODE = 'en-us'
+
 TIME_ZONE = 'UTC'
+
 USE_I18N = True
+
 USE_TZ = True
 
-# --------------------
-# Static files
-# --------------------
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_STORAGE = 'pipeline.storage.PipelineCachedStorage'
 
@@ -133,9 +130,6 @@ PIPELINE = {
     },
 }
 
-# --------------------
-# Firebase initialization
-# --------------------
 FIREBASE_CONFIG = {
     "apiKey": config("FIREBASE_API_KEY"),
     "authDomain": config("FIREBASE_AUTH_DOMAIN"),
@@ -145,15 +139,21 @@ FIREBASE_CONFIG = {
     "appId": config("FIREBASE_APP_ID"),
     "measurementId": config("FIREBASE_MEASUREMENT_ID"),
 }
+from decouple import config
+
+FIREBASE_API_KEY = config("FIREBASE_API_KEY")
+FIREBASE_AUTH_DOMAIN = config("FIREBASE_AUTH_DOMAIN")
+FIREBASE_PROJECT_ID = config("FIREBASE_PROJECT_ID")
+FIREBASE_STORAGE_BUCKET = config("FIREBASE_STORAGE_BUCKET")
+FIREBASE_MESSAGING_SENDER_ID = config("FIREBASE_MESSAGING_SENDER_ID")
+FIREBASE_APP_ID = config("FIREBASE_APP_ID")
+FIREBASE_MEASUREMENT_ID = config("FIREBASE_MEASUREMENT_ID")
 
 if not firebase_admin._apps:
-    cred_path = config("FIREBASE_SERVICE_ACCOUNT")  # path to JSON file
+    cred_path = config("FIREBASE_SERVICE_ACCOUNT")
     cred = credentials.Certificate(cred_path)
     firebase_admin.initialize_app(cred)
 
-# --------------------
-# Email configuration
-# --------------------
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
@@ -161,7 +161,3 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
-# --------------------
-# Default primary key
-# --------------------
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
